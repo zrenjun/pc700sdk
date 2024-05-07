5356a107ba563dfcfbe41249f475e99ad25a4220
周仁军 <zhourenjun1@lepucloud.com>
2024-05-07 10:21:53 +0800

    签名替换

--- app/src/main/java/com/lepu/pc700/MainFragment.kt
+++ app/src/main/java/com/lepu/pc700/MainFragment.kt
@@ -3,6 +3,7 @@ package com.lepu.pc700
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.lepu.pc700.databinding.FragmentMainBinding
 import com.lepu.pc700.utils.singleClick
 import com.lepu.pc700.utils.viewBinding
@@ -16,7 +17,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val binding by viewBinding(FragmentMainBinding::bind)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.tv1.singleClick {
@@ -32,11 +33,8 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
         }
         binding.tv5.singleClick {
-
+            findNavController().navigate(R.id.settingFragment)
         }
-
-
-
     }
 }
 
